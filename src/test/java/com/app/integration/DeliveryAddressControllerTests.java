5c20c9b35d7236663c8b05fb5a0ee44b6420c1b6
obsidoco <primakrakow@interia.pl>
2019-10-09 12:57:28 +0200

    Delivery address tests

---
+++ src/test/java/com/app/integration/DeliveryAddressControllerTests.java
@@ -0,0 +1,49 @@
+package com.app.integration;
+
+import com.app.PrimaPlatformaApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = PrimaPlatformaApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.tests.properties")
+public class DeliveryAddressControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("all")
+    void test1() throws Exception {
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/deliveryAddress/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+    @Test
+    @DisplayName("add")
+    void test2() throws Exception {
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/deliveryAddress/add"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
