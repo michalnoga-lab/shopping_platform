1dfb9e627777395ea167c0cad40a06a55da94881
obsidoco <primakrakow@interia.pl>
2020-01-14 13:11:23 +0100

    User add form and validation

--- src/main/java/com/app/validators/UserValidator.java
+++ src/main/java/com/app/validators/UserValidator.java
@@ -19,11 +19,27 @@ public class UserValidator implements Validator {
     public void validate(Object o, Errors errors) {
         UserDTO userDTO = (UserDTO) o;
         final String USER_NAME_SURNAME_REGEX = "[a-zA-z\\s-ąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+";
-        final String PASSWORD_REGEX = "[.]{8,50}";
+        final String PASSWORD_REGEX = "(.){8,50}";
 
         try {
+            if (!userDTO.getLogin().matches(USER_NAME_SURNAME_REGEX)) { // TODO: 14.01.2020 check if login is unique
+                errors.rejectValue("login", "NIEPRAWIDŁOWA WARTOŚĆ. DOZWOLONE SĄ TYLKO LITERY.");
+            }
             if (!userDTO.getName().matches(USER_NAME_SURNAME_REGEX)) {
                 errors.rejectValue("name", "NIEPRAWIDŁOWA WARTOŚĆ. DOZWOLONE SĄ TYLKO LITERY.");
+            }
+            if (!userDTO.getSurname().matches(USER_NAME_SURNAME_REGEX)) {
+                errors.rejectValue("surname", "NIEPRAWIDŁOWA WARTOŚĆ. DOZWOLONE SĄ TYLKO LITERY.");
+            }
+            if (!userDTO.getPassword().matches(PASSWORD_REGEX)) { // TODO: 14.01.2020 check if passwords are the same
+                errors.rejectValue("password",
+                        "MINIMALNA DŁUGOŚĆ HASŁA 8 ZNAKÓW. MAKSYMALNA 50 ZNAKÓW.\n" +
+                                "HASŁO ORAZ JEGO POTWIERDZENIE MUSZĄ BYĆ TAKIE SAME.\nDOZWOLONE SĄ WSZYSTKIE ZNAKI.");
+            }
+            if (!userDTO.getPasswordConfirmation().matches(PASSWORD_REGEX)) {
+                errors.rejectValue("passwordConfirmation",
+                        "MINIMALNA DŁUGOŚĆ HASŁA 8 ZNAKÓW. MAKSYMALNA 50 ZNAKÓW\n" +
+                                "HASŁO ORAZ JEGO POTWIERDZENIE MUSZĄ BYĆ TAKIE SAME.\nDOZWOLONE SĄ WSZYSTKIE ZNAKI.");
             }
         } catch (Exception e) {
             throw new AppException(ExceptionCodes.VALIDATION, "UserDto");
