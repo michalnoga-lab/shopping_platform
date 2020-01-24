5b6c4b336aeb99a009081108f38c5c99bb4547b1
obsidoco <primakrakow@interia.pl>
2020-01-24 22:03:17 +0100

    AdminProductsControllerTests - all

---
+++ src/test/java/com/app/integration/admin/AdminProductControllerTests.java
@@ -0,0 +1,44 @@
+package com.app.integration.admin;
+
+import com.app.PrimaPlatformaApplication;
+import com.app.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = PrimaPlatformaApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.tests.properties")
+public class AdminProductControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    @DisplayName("all")
+    void test1() throws Exception {
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/admin/products/all").contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
--- src/test/java/com/app/integration/web/ProductControllerTests.java
+++ src/test/java/com/app/integration/web/ProductControllerTests.java
@@ -1,18 +1,31 @@
 package com.app.integration.web;
 
 import com.app.PrimaPlatformaApplication;
+import com.app.dto.CartDTO;
+import com.app.dto.ProductDTO;
+import com.app.mappers.ProductMapper;
+import com.app.model.Product;
+import com.app.service.CartService;
+import com.app.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 
@@ -27,6 +40,12 @@ public class ProductControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ProductService productService;
+
+    @MockBean
+    private CartService cartService;
+
     @Test
     @DisplayName("all")
     void test1() throws Exception {
@@ -39,10 +58,16 @@ public class ProductControllerTests {
 
     @Test
     @DisplayName("buy/{id}")
-    void test2() throws Exception { // TODO: 2020-01-15 ID filed
+    void test2() throws Exception {
+
+        ProductDTO product = ProductDTO.builder().id(1L).build();
+        Mockito
+                .when(productService.getOneProduct(1L))
+                .thenReturn(product);
 
         mockMvc
-                .perform(MockMvcRequestBuilders.get("/products/buy"))
+                .perform(MockMvcRequestBuilders.get("/products/buy/{id}", 1L)
+                        .contentType(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
@@ -51,71 +76,23 @@ public class ProductControllerTests {
     @DisplayName("buy")
     void test3() throws Exception {
 
-        // TODO: 2020-01-15 org.springframework.web.util.NestedServletException:
-        //  Request processing failed; nested exception is AppException{id=null, exceptionCode=VALIDATION, description='ProductDTO'}
+        ProductDTO productDTO = ProductDTO.builder().id(2L).build();
+        ProductDTO addedProduct = ProductDTO.builder().id(2L).build();
+        CartDTO cartDTO = CartDTO.builder().id(3L).build();
 
+        Mockito
+                .when(cartService.addProductToCart(productDTO, 2L))
+                .thenReturn(cartDTO);
+
+        // TODO: 2020-01-24 org.springframework.web.util.NestedServletException:
+        //  Request processing failed; nested exception is AppException{id=null, exceptionCode=VALIDATION, description='ProductDTO'}
         mockMvc
-                .perform(MockMvcRequestBuilders.post("/products/buy"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .perform(MockMvcRequestBuilders.post("/products/buy")
+                ).andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
-}
 
-/**
- * @ExtendWith(SpringExtension.class)
- * @WebMvcTest(ProductController.class) public class ControllersTests {
- * @MockBean private ProductService productService;
- * @Autowired private MockMvc mockMvc;
- * @Test void test1() throws Exception {
- * <p>
- * Product p1 = Product.builder().name("A").quantity(1).build();
- * Product p2 = Product.builder().name("B").quantity(2).build();
- * Product p3 = Product.builder().name("C").quantity(3).build();
- * List<Product> products = List.of(p1, p2, p3);
- * <p>
- * Mockito.when(productService.findAll()).thenReturn(products);
- * <p>
- * mockMvc
- * .perform(MockMvcRequestBuilders
- * .get("/products")
- * .param("req1", "value1")
- * .contentType(MediaType.APPLICATION_JSON))
- * .andExpect(MockMvcResultMatchers.status().isOk())
- * .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)))
- * .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.equalTo("A")));
- * <p>
- * }
- * @Test void test2() throws Exception {
- * <p>
- * Product p1 = Product.builder().id(1L).name("A").quantity(1).build();
- * Mockito.when(productService.findOne(1L)).thenReturn(p1);
- * <p>
- * mockMvc
- * .perform(MockMvcRequestBuilders
- * .get("/products/{id}", 1L)
- * .contentType(MediaType.APPLICATION_JSON))
- * .andExpect(MockMvcResultMatchers.status().isOk())
- * .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.equalTo("A")));
- * <p>
- * }
- * @Test void test3() throws Exception {
- * <p>
- * Product p1 = Product.builder().name("A").quantity(1).build();
- * Product p11 = Product.builder().id(1L).name("A").quantity(1).build();
- * Mockito.when(productService.add(p1)).thenReturn(p11);
- * <p>
- * var res = mockMvc
- * .perform(MockMvcRequestBuilders
- * .post("/products")
- * .content(toJson(p1))
- * .contentType(MediaType.APPLICATION_JSON))
- * .andExpect(MockMvcResultMatchers.status().isCreated())
- * .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo(1)));
- * <p>
- * System.out.println(res.andReturn().getResponse().getStatus());
- * }
- * <p>
- * private static String toJson(Product product) throws JsonProcessingException {
- * return new ObjectMapper().writeValueAsString(product);
- * }
- */
+    private static String toJson(ProductDTO productDTO) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(productDTO);
+    }
+}
