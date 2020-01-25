f9d5ef00b6d490914c1fb24504cbff8c975be944
obsidoco <primakrakow@interia.pl>
2020-01-25 17:32:28 +0100

    AdminPanel tests - all

--- src/main/java/com/app/adminControllers/PanelController.java
+++ src/main/java/com/app/adminControllers/PanelController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("admin")
-public class Panel {
+public class PanelController {
 
     @GetMapping("panel")
     public String adminShowPanel() {
