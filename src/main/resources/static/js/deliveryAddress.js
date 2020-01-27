75629f8627366393878fd622edbe77c0a2753ef6
obsidoco <primakrakow@interia.pl>
2020-01-27 21:27:52 +0100

    Cartcontroller tests

--- src/main/resources/static/js/deliveryAddress.js
+++ src/main/resources/static/js/deliveryAddress.js
@@ -1,19 +1,28 @@
-const streetRegex = new RegExp('[a-zA-z\\s-ąćęłńóśźżĄĆĘŁŃÓŚŹŻ0-9]+');
+const streetRegex = new RegExp('abc', 'g');
 const street = document.getElementById('street');
 
 const checkStreet = () => {
     const streetFeedback = document.getElementById('streetFeedback');
 
-    if (street.innerText.length <= 0) {
+    if (street.innerText === '') {
         streetFeedback.textContent = "KROTKI !!!!";
     }
 
-    if (!streetRegex.test(street.innerText)) {
-        const response = 'PODANY ADRES NIE JEST PRAWIDŁOWY';
+    console.log('---------------------');
+    console.log(street.innerText);
+    console.log(street);
+    console.log(street.innerHTML);
+    console.log(street.inputMode);
+    console.log(street.accessKey);
+
+    if (streetRegex.test(street.accessKey)) {
+        const response = 'PODANY ADRES NIE JEST PRAWIDŁOWY 00000000000000000';
+        response.fontcolor('red');
         streetFeedback.textContent = response;
     } else {
         streetFeedback.textContent = '';
     }
 };
-
 street.addEventListener('blur', checkStreet);
+street.addEventListener('change', checkStreet);
+street.addEventListener('input', checkStreet);
--- src/test/java/com/app/integration/CartControllerTests.java
+++ src/test/java/com/app/integration/CartControllerTests.java
@@ -1,18 +1,29 @@
 package com.app.integration;
 
 import com.app.PrimaPlatformaApplication;
+import com.app.controllers.CartController;
+import com.app.dto.CartDTO;
+import com.app.mappers.CartMapper;
+import com.app.model.Cart;
+import com.app.repository.CartRepository;
+import com.app.service.CartService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 
@@ -26,6 +37,12 @@ public class CartControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private CartRepository cartRepository;
+
+    @MockBean
+    private CartService cartService;
 
     @Test
     @DisplayName("all")
@@ -41,8 +58,17 @@ public class CartControllerTests {
     @DisplayName("/one/{id}")
     void test2() throws Exception { // TODO: 2020-01-12
 
+        CartDTO cart = CartDTO.builder().id(1L).build();
+        Mockito
+                .when(cartService.getCart(1L))
+                .thenReturn(cart);
+
+        Mockito
+                .when(cartRepository.findById(1L))
+                .thenReturn(Optional.of(CartMapper.fromDto(cart)));
+
         mockMvc
-                .perform(MockMvcRequestBuilders.get("/carts/one/").param("id", "1"))
+                .perform(MockMvcRequestBuilders.get("/carts/one/{id}", 1L))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
@@ -53,6 +79,16 @@ public class CartControllerTests {
 
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/carts/one/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+    @Test
+    @DisplayName("closed")
+    void test4() throws Exception {
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/carts/closed").contentType(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
