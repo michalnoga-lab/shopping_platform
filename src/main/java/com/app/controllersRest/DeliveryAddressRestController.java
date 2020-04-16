bb412ffa48f2e344305a034b7095ee3f1a79c360
obsidoco <primakrakow@interia.pl>
2020-04-16 11:25:58 +0200

    Delivery Address REST Controller all methods

--- src/main/java/com/app/controllersRest/DeliveryAddressRestController.java
+++ src/main/java/com/app/controllersRest/DeliveryAddressRestController.java
@@ -1,7 +1,8 @@
 package com.app.controllersRest;
 
+import com.app.dto.CartDTO;
 import com.app.dto.DeliveryAddressDTO;
-import com.app.mappers.DeliveryAddressMapper;
+import com.app.service.CartService;
 import com.app.service.DeliveryAddressService;
 import com.app.service.SecurityService;
 import lombok.RequiredArgsConstructor;
@@ -18,20 +19,29 @@ import java.util.List;
 public class DeliveryAddressRestController {
 
     private final DeliveryAddressService deliveryAddressService;
+    private final CartService cartService;
     private final SecurityService securityService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<DeliveryAddressDTO>> all() {
-        // TODO: 31.03.2020 przekazać użytkownika
-        return new ResponseEntity<>(deliveryAddressService.findAll(null), HttpStatus.OK);
+    public ResponseEntity<List<DeliveryAddressDTO>> all(HttpServletRequest request) {
+        return new ResponseEntity<>(deliveryAddressService.findAll(securityService.getLoggedInUserId(
+                request.getAttribute("username").toString())), HttpStatus.OK);
     }
 
     @PostMapping("add")
     public ResponseEntity<DeliveryAddressDTO> add(@RequestBody DeliveryAddressDTO deliveryAddressDTO, HttpServletRequest request) {
-        Long userId = securityService.getLoggedInUserId((String) request.getAttribute("username"));
-        return new ResponseEntity<>(deliveryAddressService.add(deliveryAddressDTO, userId), HttpStatus.OK);
+        return new ResponseEntity<>(deliveryAddressService.add(deliveryAddressDTO,
+                securityService.getLoggedInUserId(request.getAttribute("username").toString())), HttpStatus.OK);
     }
 
-    // TODO: 2020-03-24 remove
-    // TODO: 2020-03-24 pick / select this one
+    @PostMapping("remove/{id}")
+    public ResponseEntity<DeliveryAddressDTO> remove(@PathVariable Long id) {
+        return new ResponseEntity<>(deliveryAddressService.hide(id), HttpStatus.OK);
+    }
+
+    @GetMapping("pick/{id}")
+    public ResponseEntity<CartDTO> pick(@PathVariable Long id, HttpServletRequest request) {
+        return new ResponseEntity<>(cartService.setAddressToCart(id,
+                securityService.getLoggedInUserId(request.getAttribute("username").toString())), HttpStatus.OK);
+    }
 }
