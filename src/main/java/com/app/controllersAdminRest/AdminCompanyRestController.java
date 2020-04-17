ff182543e38ccd249f41dcc2474a7f4c9a57c99c
obsidoco <primakrakow@interia.pl>
2020-04-17 09:09:06 +0200

    Admin Company REST Controller - all methods

--- src/main/java/com/app/controllersAdminRest/AdminCompanyRestController.java
+++ src/main/java/com/app/controllersAdminRest/AdminCompanyRestController.java
@@ -1,8 +1,48 @@
 package com.app.controllersAdminRest;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.app.dto.CompanyDTO;
+import com.app.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("api/admin/companies")
 public class AdminCompanyRestController {
-    // TODO: 2020-03-24  
+
+    private final CompanyService companyService;
+
+    @PostMapping("/add")
+    public ResponseEntity<CompanyDTO> add(@ModelAttribute CompanyDTO companyDTO) {
+        return new ResponseEntity<>(companyService.add(companyDTO), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CompanyDTO>> adminAll() {
+        return new ResponseEntity<>(companyService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/one/{id}")
+    public ResponseEntity<CompanyDTO> one(@PathVariable Long id) {
+        return new ResponseEntity<>(companyService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/edit/{id}")
+    public ResponseEntity<CompanyDTO> edit(@PathVariable Long id) { // TODO: 17.04.2020 jeszcze ModelAttribute z company DTO
+        return new ResponseEntity<>(companyService.edit(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/enable/{id}")
+    public ResponseEntity<CompanyDTO> enable(@PathVariable Long id) {
+        return new ResponseEntity<>(companyService.disableEnable(id, true), HttpStatus.OK);
+    }
+
+    @PostMapping("/disable/{id}")
+    public ResponseEntity<CompanyDTO> disable(@PathVariable Long id) {
+        return new ResponseEntity<>(companyService.disableEnable(id, false), HttpStatus.OK);
+    }
 }
