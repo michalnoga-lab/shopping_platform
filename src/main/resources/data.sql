143519f53dbffbb881ebea4efbef388e249c4bea
Michal <michalnoga.poczta@gmail.com>
2020-10-31 21:36:40 +0100

    Initial DB data updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@ values (1, 'UJGO');
 
 INSERT INTO companies (id, active, city, default_price, name, name_shortcut, nip, payment_in_days, post_code, street,
                        street_number, company_code_id)
-VALUES (2, true, 'krakow', 'NET', 'name', 'shortcut', '0000000000', 30, '00-000', 'street', 50, 1),
-       (11, true, 'krakow', 'NET', 'name', 'shortcut', '0000000000', 30, '00-000', 'street', 50, 1);
+VALUES (2, true, 'krakow', 'NET', 'name_2', 'shortcut', '0000000000', 30, '00-000', 'street', 50, 1),
+       (11, true, 'krakow', 'NET', 'name_11', 'shortcut', '0000000000', 30, '00-000', 'street', 50, 1);
 
 INSERT INTO products (id, auction_index, description, gross_price, hidden, name, nett_price, number_in_auction,
                       vat, company_id, product_code_id)
@@ -19,7 +19,7 @@ INSERT INTO users (id, email, enabled, login, name, password, role, surname, com
 VALUES (6, 'user@gmail.com', true, 'user@gmail.com', 'name',
         '{bcrypt}$2a$10$/HxZgKD8i8uVvtbyMcYPkeeybREyK72tEtVV25OxPvufeUSt9fFEa', 'USER', 'surname', 2),
        (7, 'admin@gmail.com', true, 'admin@gmail.com', 'name',
-        '{bcrypt}$2a$10$/HxZgKD8i8uVvtbyMcYPkeeybREyK72tEtVV25OxPvufeUSt9fFEa', 'USER', 'surname', 11);
+        '{bcrypt}$2a$10$/HxZgKD8i8uVvtbyMcYPkeeybREyK72tEtVV25OxPvufeUSt9fFEa', 'ADMIN', 'surname', 11);
 
 INSERT INTO delivery_addresses (id, hidden, phone, street, user_id)
 VALUES (10, false, '1121212121', 'street_1', 6);
