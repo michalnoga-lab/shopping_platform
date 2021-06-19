1360de07c940e7176c5a99ab86d8bbcfd6d2684a
Michal <michalnoga.poczta@gmail.com>
2021-06-19 19:22:00 +0200

    Login SQL as preapred statement

--- pages/login.php
+++ pages/login.php
@@ -15,47 +15,45 @@ session_start();
 <body>
 <script defer src="../static/js/login/validation.js"></script>
 <div class="container">
-    <div class="row">
-        <?php
-        if (isset($_GET['error'])) {
-            $errorCheck = $_GET['error'];
+    <?php
+    if (isset($_GET['info'])) {
+        $infoCheck = $_GET['info'];
 
-            if ($errorCheck === 'stmt_failed') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błąd połączenia</div>');
-            } elseif ($errorCheck === 'user_not_found') {
-                echo('<div class="alert alert-danger text-center" role="alert">Nie odnaleziono użytkownika o takim adresie email</div>');
-            } elseif ($errorCheck === 'invalid_password') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błędne hasło</div>');
-            } elseif ($errorCheck === 'empty_input') {
-                echo('<div class="alert alert-danger text-center" role="alert">Nie wszystkie pola są wypełnione</div>');
-            }
+        if ($infoCheck === 'stmt_failed') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błąd połączenia</div>');
+        } elseif ($infoCheck === 'user_not_found') {
+            echo('<div class="alert alert-danger text-center" role="alert">Nie odnaleziono użytkownika o takim adresie email</div>');
+        } elseif ($infoCheck === 'invalid_password') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błędne hasło</div>');
+        } elseif ($infoCheck === 'empty_input') {
+            echo('<div class="alert alert-danger text-center" role="alert">Nie wszystkie pola są wypełnione</div>');
         }
-        ?>
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card border-0 shadow rounded-3 my-5">
-                <div class="card-body p-4 p-sm-5">
-                    <h5 class="card-title text-center mb-5 fw-light fs-5">Logowanie</h5>
-                    <form action="/includes/login.inc.php" method="post" id="form">
-                        <div class="form-floating mb-3 input-control">
-                            <input type="email" class="form-control" id="email" name="email"
-                                   placeholder="Adres email">
-                            <label for="email">Adres email</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="form-floating mb-3 input-control">
-                            <input type="password" class="form-control" id="password" name="password"
-                                   placeholder="Hasło">
-                            <label for="password">Hasło</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="d-grid">
-                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" name="submit"
-                                    id="submit">Logowanie
-                            </button>
-                        </div>
-                        <hr class="my-4">
-                    </form>
-                </div>
+    }
+    ?>
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+            <div class="card-body p-4 p-sm-5">
+                <h5 class="card-title text-center mb-5 fw-light fs-5">Logowanie</h5>
+                <form action="/includes/login.inc.php" method="post" id="form">
+                    <div class="form-floating mb-3 input-control">
+                        <input type="email" class="form-control" id="email" name="email"
+                               placeholder="Adres email">
+                        <label for="email">Adres email</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="form-floating mb-3 input-control">
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Hasło">
+                        <label for="password">Hasło</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" name="submit"
+                                id="submit">Logowanie
+                        </button>
+                    </div>
+                    <hr class="my-4">
+                </form>
             </div>
         </div>
     </div>
