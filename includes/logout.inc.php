700384e46862a83ce24513bc7a960958c3d4905d
Michal <michalnoga.poczta@gmail.com>
2021-06-20 12:36:43 +0200

    Logout successful registration

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,8 +1,13 @@
 <?php
 // error_reporting(0); // TODO
 
+include_once './logger.inc.php';
+
+$logger = new Logger();
+$logger->systemEvent('Logout successful with email ' . $_SESSION['email']);
+
 session_start();
 session_unset();
 session_destroy();
 
-header('location: ../index.php?error=logged_out'); // TODO zapis do logów
+header('location: ../index.php?error=logged_out');
