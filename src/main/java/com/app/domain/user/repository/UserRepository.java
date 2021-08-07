a2a0ca596854c4ffbe2aac5c59a54cc262b02893
Michal <michalnoga.poczta@gmail.com>
2021-08-07 22:32:08 +0200

    UserRepository added

---
+++ src/main/java/com/app/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.app.domain.user.repository;
+
+import com.app.domain.repository.CrudRepository;
+import com.app.domain.user.User;
+
+/**
+ * UserRepository interface
+ * Provides all methods from CrudRepository interface to use with User
+ *
+ * @see CrudRepository
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+}
