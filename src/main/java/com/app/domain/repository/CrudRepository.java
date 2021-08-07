5c2487d3724818664aa095d6c095478c91699020
Michal <michalnoga.poczta@gmail.com>
2021-08-07 22:21:32 +0200

    CRUD repository interface added

---
+++ src/main/java/com/app/domain/repository/CrudRepository.java
@@ -0,0 +1,36 @@
+package com.app.domain.repository;
+
+import java.util.Optional;
+
+/**
+ * CrudRepository interface
+ *
+ * @param <T>  is generic object type injecting into repository
+ * @param <ID> is object ID in database
+ */
+public interface CrudRepository<T, ID> {
+
+    /**
+     * This method adds T object to repository
+     *
+     * @param t is object to save
+     * @return optional of added object
+     */
+    Optional<T> add(T t);
+
+    /**
+     * This method removes object from repository
+     *
+     * @param id is ID of object to remove
+     * @return optional od removed object
+     */
+    Optional<T> delete(ID id);
+
+    /**
+     * This method returns matching object based on given ID
+     *
+     * @param id is object ID we are looking for
+     * @return founded object or empty optional if object is not present
+     */
+    Optional<T> findById(ID id);
+}
