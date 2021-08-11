aecd36eea45c7b41d5b3508773ea8782e0ee4e48
Michal <michalnoga.poczta@gmail.com>
2021-08-11 20:27:50 +0200

    Dependencies updated with security, password encoder, and gson

---
+++ src/main/java/com/app/application/service/UsersService.java
@@ -0,0 +1,14 @@
+package com.app.application.service;
+
+import com.app.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsersService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+}
