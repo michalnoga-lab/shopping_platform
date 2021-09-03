64761fe6260df8815da2c2edd4d72d9286b85a1a
Michal <michalnoga.poczta@gmail.com>
2021-09-03 20:41:42 +0200

    RODO policy page

---
+++ pages/rodo.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(0);
+session_start();
+?>
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <?php include_once('../static/elements/head.inc.php'); ?>
+    <title>Prima Platforma</title>
+</head>
+<body>
+
+<div class="col-12">
+    <div class="h-100 p-5 bg-light border rounded-3">
+        <h2>Polityka RODO</h2>
+        <p> Informujemy, że administratorem Pani/Pana danych osobowych jest Firma Handlowa PRIMA z siedzibą
+            w Krakowie, ul. Półłanki 31G.</p>
+        <p> Pani/Pana dane osobowe będą przetwarzane w celu umożliwienia kontynuacji
+            współpracy pomiędzy Firmą Handlowa PRIMA a Panem/Panią oraz w celu marketingu bezpośredniego
+            realizowanego przez Firma Handlowa PRIMA. Przetwarzanie danych osobowych jest niezbędne do
+            realizacji wskazanych wyżej celów oraz odpowiadających tym celom prawnie uzasadnionych interesów
+            administratora. Podanie danych osobowych jest dobrowolne, jednak niezbędne do wykonywania w/w
+            czynności. Pomimo dobrowolności, konsekwencją niepodania danych osobowych będzie brak możliwości
+            wykonywania w/w czynności.</p>
+        <p> Podstawą prawną przetwarzania podanych przez Panią/Pana danych osobowych są przepisy art. 6 ust.
+            1 lit. b i f ROZPORZĄDZENIA PARLAMENTU EUROPEJSKIEGO I RADY (UE) 2016/679 z dnia 27 kwietnia
+            2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w
+            sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE, a w pozostałym
+            zakresie – udzielona przez Państwa zgoda.</p>
+        <p>Pani/Pan prawo do wniesienia skargi do organu nadzorczego w zakresie ochrony danych osobowych,
+            jeśli uzna Pani/Pan, że przetwarzanie danych osobowych przez Firmę Handlową PRIMA narusza
+            przepisy o ochronie danych osobowych. Przetwarzane przez nas Pani/Pana dane osobowe nie będą w
+            żadnym stopniu przekazywane do osób/Państw trzecich.</p>
+        <p>Przysługuje Pani/Panu prawo dostępu do treści swoich danych osobowych oraz prawo do ich
+            sprostowania, usunięcia lub ograniczenia przetwarzania, prawo do przenoszenia danych oraz prawo
+            do wniesienia sprzeciwu wobec ich przetwarzania w dowolnym momencie bez wpływu na zgodność z
+            prawem przetwarzania, którego dokonano na podstawie zgody przed jej cofnięciem. W zakresie, w
+            jakim wykorzystanie podanych przez Panią/Pana informacji opiera się na udzielonej przez
+            Panią/Pana zgodzie, ma Pani/Pan prawo do cofnięcia zgody w dowolnym momencie bez wpływu na
+            zgodność z prawem przetwarzania, którego dokonano na podstawie zgody przed jej cofnięciem.</p>
+        <p>Jeśli chce Pani/Pan zmienić zakres przetwarzanych przez nas danych, w tym wnieść sprzeciw lub
+            cofnąć swoją zgodę, prosimy o wysłanie wiadomości na adres e-mail: biuro@primakrakow.pl</p>
+
+        <a href="../index.php" class="btn btn-outline-secondary" type="button">Powrót do strony głównej</a>
+    </div>
+</div>
+
+<footer>
+    <?php include_once('../static/elements/footer.php') ?>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous">
+</script>
+</body>
+</html>
