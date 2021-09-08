e57615750e7c6cec625ce18f7507f4aa22326943
Michal <michalnoga.poczta@gmail.com>
2021-09-08 12:04:08 +0200

    Minor tests updates

--- src/test/java/com/app/domain/cart_product/repository/CartProductRepositoryTests.java
+++ src/test/java/com/app/domain/cart_product/repository/CartProductRepositoryTests.java
@@ -1,15 +1,13 @@
-package com.app.domain.verification_token.repository;
+package com.app.domain.cart_product.repository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class VerificationTokenRepositoryTests {
+public class CartProductRepositoryTests {
 
     @Test
     void contextLoads() {
     }
-
-    // TODO czy tu się robi testy
 }
--- src/test/java/com/app/domain/user/repository/UserRepositoryTests.java
+++ src/test/java/com/app/domain/user/repository/UserRepositoryTests.java
@@ -1,13 +1,15 @@
-package com.app.domain.user.role;
+package com.app.domain.user.repository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class RoleTests {
+public class UserRepositoryTests {
 
     @Test
     void contextLoads() {
     }
+
+    // TODO czy tu się robi testy
 }
