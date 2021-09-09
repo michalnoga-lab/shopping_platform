7fc1954a5b26296f03dd7b19d3a363b863eb4230
Michal <michalnoga.poczta@gmail.com>
2021-09-09 16:11:00 +0200

    register page updated

--- pages/register.php
+++ pages/register.php
@@ -15,70 +15,68 @@ session_start();
 <body>
 <script defer src="../static/js/register/validation.js"></script>
 <div class="container">
-    <div class="row">
-        <?php
-        if (isset($_GET['error'])) {
-            $errorCheck = $_GET['error'];
+    <?php
+    if (isset($_GET['error'])) {
+        $errorCheck = $_GET['error'];
 
-            if ($errorCheck === 'empty_input') {
-                echo '<div class="alert alert-danger text-center" role="alert">Nie podano danych do rejestracji</div>';
-            } elseif ($errorCheck === 'invalid_username') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błędna nazwa użytkownika</div>');
-            } elseif ($errorCheck === 'invalid_email') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błędny adres email</div>');
-            } elseif ($errorCheck === 'invalid_password') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błędne hasło</div>');
-            } elseif ($errorCheck === 'invalid_confirmation') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błędne potwierdzenie hasła</div>');
-            } elseif ($errorCheck === 'different_passwords') {
-                echo('<div class="alert alert-danger text-center" role="alert">Podane hasła nie są takie same</div>');
-            } elseif ($errorCheck === 'email_in_db') {
-                echo('<div class="alert alert-danger text-center" role="alert">Taki email jest już w użyciu. Proszę wybrać inny</div>');
-            } elseif ($errorCheck === 'connection') {
-                echo('<div class="alert alert-danger text-center" role="alert">Błąd połączenia</div>');
-            }
+        if ($errorCheck === 'empty_input') {
+            echo '<div class="alert alert-danger text-center" role="alert">Nie podano danych do rejestracji</div>';
+        } elseif ($errorCheck === 'invalid_username') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błędna nazwa użytkownika</div>');
+        } elseif ($errorCheck === 'invalid_email') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błędny adres email</div>');
+        } elseif ($errorCheck === 'invalid_password') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błędne hasło</div>');
+        } elseif ($errorCheck === 'invalid_confirmation') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błędne potwierdzenie hasła</div>');
+        } elseif ($errorCheck === 'different_passwords') {
+            echo('<div class="alert alert-danger text-center" role="alert">Podane hasła nie są takie same</div>');
+        } elseif ($errorCheck === 'email_in_db') {
+            echo('<div class="alert alert-danger text-center" role="alert">Taki email jest już w użyciu. Proszę wybrać inny</div>');
+        } elseif ($errorCheck === 'connection') {
+            echo('<div class="alert alert-danger text-center" role="alert">Błąd połączenia</div>');
         }
-        ?>
+    }
+    ?>
 
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card border-0 shadow rounded-3 my-5">
-                <div class="card-body p-4 p-sm-5">
-                    <h5 class="card-title text-center mb-5 fw-light fs-5">Rejestracja</h5>
-                    <form action="/includes/register.inc.php" method="post" id="form">
-                        <div class="form-floating mb-3 input-control">
-                            <input type="text" class="form-control" id="username" name="username"
-                                   placeholder="Imię i nazwisko"
-                                   required>
-                            <label for="username">Imię i Nazwisko</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="form-floating mb-3 input-control">
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Adres email"
-                                   required>
-                            <label for="email">Adres email</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="form-floating mb-3 input-control">
-                            <input type="password" class="form-control" id="password" name="password"
-                                   placeholder="Hasło" required>
-                            <label for="password">Hasło</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="form-floating mb-3 input-control">
-                            <input type="password" class="form-control" id="password-confirmation"
-                                   name="passwordConfirmation"
-                                   placeholder="Powtórz hasło" required>
-                            <label for="password-confirmation">Powtórz hasło</label>
-                            <div class="error"></div>
-                        </div>
-                        <div class="d-grid">
-                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" id="submit"
-                                    name="submit">Zarejestruj konto
-                            </button>
-                        </div>
-                        <hr class="my-4">
-                    </form>
-                </div>
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+            <div class="card-body p-4 p-sm-5">
+                <h5 class="card-title text-center mb-5 fw-light fs-5">Rejestracja</h5>
+                <form action="/includes/register.inc.php" method="post" id="form">
+                    <div class="form-floating mb-3 input-control">
+                        <input type="text" class="form-control" id="username" name="username"
+                               placeholder="Imię i nazwisko"
+                               required>
+                        <label for="username">Imię i Nazwisko</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="form-floating mb-3 input-control">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Adres email"
+                               required>
+                        <label for="email">Adres email</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="form-floating mb-3 input-control">
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Hasło" required>
+                        <label for="password">Hasło</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="form-floating mb-3 input-control">
+                        <input type="password" class="form-control" id="password-confirmation"
+                               name="passwordConfirmation"
+                               placeholder="Powtórz hasło" required>
+                        <label for="password-confirmation">Powtórz hasło</label>
+                        <div class="error"></div>
+                    </div>
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" id="submit"
+                                name="submit">Zarejestruj konto
+                        </button>
+                    </div>
+                    <hr class="my-4">
+                </form>
             </div>
         </div>
     </div>
