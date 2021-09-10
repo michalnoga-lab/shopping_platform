0e7859a556bb3ef0d1dc84046f35089b63433429
Michal <michalnoga.poczta@gmail.com>
2021-09-10 21:44:51 +0200

    Tokens Service Values

--- src/main/java/com/app/infrastructure/security/tokens/AppTokensService.java
+++ src/main/java/com/app/infrastructure/security/tokens/AppTokensService.java
@@ -1,4 +1,18 @@
 package com.app.infrastructure.security.tokens;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AppTokensService {
+
+    @Value("${tokens.access-token.expiration-time-ms}")
+    private Long accessTokenExpirationTimeMs;
+
+    @Value("${tokens.refresh-token.expiration-time-ms}")
+    private Long refreshTokenExpirationTimeMs;
+
+    // TODO tutaj zaczac
 }
