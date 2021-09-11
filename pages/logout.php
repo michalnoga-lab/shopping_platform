5506c2d6aa5d3f9ada993abc7d5949967786a730
Michal <michalnoga.poczta@gmail.com>
2021-09-11 20:42:27 +0200

    Logout page

---
+++ pages/logout.php
@@ -0,0 +1,43 @@
+<?php
+// TODO errors
+session_start();
+?>
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <?php include_once('../static/elements/head.inc.php'); ?>
+    <title>Prima Platforma</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card border-0 shadow rounded-3 my-5">
+                <div class="card-body p-4 p-sm-5">
+                    <h5 class="card-title text-center mb-5 fw-light fs-5">Koniec pracy ?</h5>
+                    <form action="/includes/logout.inc.php" method="post">
+                        <div class="d-grid">
+                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Wyloguj mnie
+                            </button>
+                        </div>
+                        <hr class="my-4">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <?php include_once('../static/elements/footer.php') ?>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous">
+</script>
+
+</body>
+</html>
