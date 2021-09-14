c782546da166c0077a1ce15a36cf9717144199e6
Michal <michalnoga.poczta@gmail.com>
2021-09-14 21:22:56 +0200

    Generate Tokens method

--- src/main/java/com/app/infrastructure/security/tokens/AppTokensService.java
+++ src/main/java/com/app/infrastructure/security/tokens/AppTokensService.java
@@ -1,11 +1,20 @@
 package com.app.infrastructure.security.tokens;
 
 import com.app.domain.user.repository.UserRepository;
+import com.app.infrastructure.security.tokens.dto.TokensDto;
+import com.app.infrastructure.security.tokens.exception.AppTokensServiceException;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static com.app.domain.user.UserUtil.toId;
 
 /**
  * Tokens service class
@@ -29,4 +38,39 @@ public class AppTokensService {
 
     private final UserRepository userRepository;
     private final SecretKey secretKey;
+
+    public TokensDto generateTokens(Authentication authentication) {
+
+        var username = authentication.getName();
+        var userId = userRepository
+                .findByEmail(username)
+                .map(toId)
+                .orElseThrow(() -> new AppTokensServiceException("Cannot find user with email " + authentication.getName()));
+        var currentDate = new Date();
+        var accessTokenExpirationDate = new Date(currentDate.getTime() + accessTokenExpirationTimeMs);
+        var refreshTokenExpirationDate = new Date(currentDate.getTime() + refreshTokenExpirationTimeMs);
+
+        var accessToken = Jwts
+                .builder()
+                .setSubject(String.valueOf(userId))
+                .setExpiration(accessTokenExpirationDate)
+                .setIssuedAt(currentDate)
+                .signWith(secretKey)
+                .compact();
+
+        var refreshToken = Jwts
+                .builder()
+                .setSubject(String.valueOf(userId))
+                .setExpiration(refreshTokenExpirationDate)
+                .setIssuedAt(currentDate)
+                .claim(refreshTokenProperty, accessTokenExpirationDate.getTime())
+                .signWith(secretKey)
+                .compact();
+
+        return TokensDto
+                .builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
