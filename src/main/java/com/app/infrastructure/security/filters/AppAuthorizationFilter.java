1248f4125d02d82484229fa461779ce97f8a7ac5
Michal <michalnoga.poczta@gmail.com>
2021-09-19 16:02:02 +0200

    Authorization filter

--- src/main/java/com/app/infrastructure/security/filters/AppAuthorizationFilter.java
+++ src/main/java/com/app/infrastructure/security/filters/AppAuthorizationFilter.java
@@ -1,7 +1,42 @@
 package com.app.infrastructure.security.filters;
 
+import com.app.infrastructure.security.tokens.AppTokensService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * App authorization filter
  */
-public class AppAuthorizationFilter {
+public class AppAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final AppTokensService appTokensService;
+
+    public AppAuthorizationFilter(AuthenticationManager authenticationManager, AppTokensService appTokensService) {
+        super(authenticationManager);
+        this.appTokensService = appTokensService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+
+        var header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header != null) {
+            var authorizedUser = appTokensService.parseAccessToken(header);
+            SecurityContextHolder.getContext().setAuthentication(authorizedUser);
+        }
+        chain.doFilter(request, response);
+    }
 }
