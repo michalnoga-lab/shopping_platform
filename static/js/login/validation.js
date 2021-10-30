31d75ec387c5faa1a967b618f5fa3db5b861fe08
Michal <michalnoga.poczta@gmail.com>
2021-10-31 00:04:09 +0200

    Login detailed validation

--- static/js/login/validation.js
+++ static/js/login/validation.js
@@ -33,25 +33,48 @@ const setSuccess = element => {
     inputControl.classList.remove('error');
 }
 
-// TODO rozpisać walidację np. zbyt długi wprowadzony ciąg itd
 const validateInputs = () => {
     const emailValue = email.value;
     const passwordValue = password.value;
+    const pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    if (emailValue === '' || emailValue.length < 1 || emailValue.length > 200 ||
-        !emailValue.match(/(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/)) {
-        setError(email, 'Proszę wprowadzić adres email');
-        isEmailValid = false;
-    } else {
-        setSuccess(email);
-        isEmailValid = true;
+    if (!pattern.test(emailValue)) {
+        setError('Podano nieprawidłowy adres email');
     }
 
-    if (passwordValue === '' || passwordValue.length < 1 || passwordValue.length > 200) {
+    if (emailValue === '' || emailValue.length < 1) {
+        setError(email, 'Proszę wprowadzić adres email');
+    }
+
+    if (emailValue.length > 200) {
+        setError(email, 'Maksymalna długość adresu to 200 znaków');
+    }
+
+    if (passwordValue === '' || passwordValue.length < 1) {
         setError(password, 'Proszę wprowadzić hasło');
+    }
+
+    if (passwordValue.length > 200) {
+        setError(password, 'Maksymalna długość hasła to 200 znaków');
+    }
+
+    if (!pattern.test(emailValue) ||
+        emailValue === '' ||
+        emailValue.length < 1 ||
+        emailValue.length > 200
+    ) {
+        isEmailValid = false;
+    } else {
+        isEmailValid = true;
+        setSuccess(email);
+    }
+
+    if (passwordValue === '' ||
+        passwordValue.length < 1 ||
+        passwordValue.length > 200) {
         isPasswordValid = false;
     } else {
-        setSuccess(password);
         isPasswordValid = true;
+        setSuccess(password);
     }
 }
