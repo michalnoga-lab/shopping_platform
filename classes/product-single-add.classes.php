4247c009db9c65d435ab7aaf602774e981da310e
Michal <michalnoga.poczta@gmail.com>
2022-01-30 22:15:00 +0100

    ProductSingleAdd updated with statement reset

--- classes/product-single-add.classes.php
+++ classes/product-single-add.classes.php
@@ -8,14 +8,10 @@ class ProductSingleAdd extends Dbh
         $stmt = $this->connect()->prepare('INSERT INTO products_in_cart (user_id, cart_id, product_id, user_comment, quantity, 
                               nett_value, vat_value, gross_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?);');
 
-        if ($stmt->execute(array($userId, $cartId, $productId, $userComment, $quantity, $nettValue, $vatValue, $grossValue))) {
-//            header('location: ../pages/products.php?info=product_saved');
-            //exit();
-        } else {
+        if (!$stmt->execute(array($userId, $cartId, $productId, $userComment, $quantity, $nettValue, $vatValue, $grossValue))) {
             $stmt = null;
             header('location: ../pages/products.php?info=product_error');
         }
         $stmt = null;
-        //exit();
     }
 }
