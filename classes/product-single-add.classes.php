ec4c5ac901855287459dfea0ac08021c7b154e52
Michal <michalnoga.poczta@gmail.com>
2022-02-27 20:29:21 +0100

    When product is in cart adding the same product to cart updates quantity

--- classes/product-single-add.classes.php
+++ classes/product-single-add.classes.php
@@ -12,6 +12,7 @@ class ProductSingleAdd extends Dbh
         if (!$stmt->execute(array($cartId, $productId))) {
             $stmt = null;
             header($this->location . 'error');
+            exit();
         }
 
         if ($stmt->rowCount() == 0) {
@@ -23,48 +24,23 @@ class ProductSingleAdd extends Dbh
                 $insert_stmt = null;
                 header($this->location . 'error');
                 exit();
-            } else {
-                $product = $stmt->fetch();
-                $product['quantity'] = $product['quantity'] + $quantity;
+            }
+        } else {
+            $product = $stmt->fetch();
+            $quantity = $product['quantity'] + $quantity;
 
-                $stmt_update = $this->connect()->prepare('UPDATE products_in_cart SET quantity = ?, nett_value = ?, vat_value = ?, gross_value = ?
+            $stmt_update = $this->connect()->prepare('UPDATE products_in_cart SET quantity = ?, nett_value = ?, vat_value = ?, gross_value = ?
                            WHERE product_id = ?;');
 
-                if (!$stmt_update->execute(array($quantity, $nettValue, $vatValue, $grossValue, $productId))) {
-                    $stmt = null;
-                    $stmt_update = null;
-                    header($this->location . 'error');
-                    exit();
-                }
-
+            if (!$stmt_update->execute(array($quantity, $nettValue, $vatValue, $grossValue, $productId))) {
                 $stmt = null;
-                header($this->location . 'product_updated');
+                $stmt_update = null;
+                header($this->location . 'error');
                 exit();
             }
+
+            $stmt = null;
         }
     }
-//    {
-//        $stmt = $this->connect()->prepare('SELECT * FROM products_in_cart WHERE cart_id = ? AND product_id = ?;');
-////        $stmt->bindParam('ii', $cartId, $productId);
-////        $stmt->execute(array($cartId, $productId));
-////        $result = $stmt->fetch();
-////
-////        print_r(json_encode($result), JSON_PRETTY_PRINT);
-////        exit();
-//
-//        if (!$result) {
-//            $stmt = $this->connect()->prepare('INSERT INTO products_in_cart (user_id, cart_id, product_id, user_comment, quantity,
-//                              nett_value, vat_value, gross_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?);');
-//
-//            if (!$stmt->execute(array($userId, $cartId, $productId, $userComment, $quantity, $nettValue, $vatValue, $grossValue))) {
-//                $stmt = null;
-//                header('location: ../pages/products.php?info=product_error');
-//            }
-//        } else {
-//            //$productFromDb = $stmt->fetch(MYSQLI_ASSOC);
-//            print_r(json_encode($result), JSON_PRETTY_PRINT);
-//            exit();
-//        }
-//        $stmt = null;
-//    }
+
 }
