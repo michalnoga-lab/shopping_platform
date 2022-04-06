5887399684b16af5b53a4afaf919e908879f814c
Michal <michalnoga.poczta@gmail.com>
2022-04-06 17:19:59 +0200

    All stylesheats updated with proper src path

--- src/static/elements/head.inc.php
+++ src/static/elements/head.inc.php
@@ -8,17 +8,17 @@ error_reporting(0);
 <meta name="description" content="Platforma zakupowa dla wybranych klientów">
 <meta name="author" content="Michał Noga OBSIDO">
 
-<link href="/static/css/reset.css" rel="stylesheet"/>
+<link href="/src/static/css/reset.css" rel="stylesheet"/>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<link href="/static/css/main.css" rel="stylesheet"/>
-<link href="/static/css/header.css" rel="stylesheet"/>
-<link href="/static/css/footer.css" rel="stylesheet"/>
-<link href="/static/css/validation.css" rel="stylesheet"/>
-<link href="/static/css/spinner.css" rel="stylesheet"/>
+<link href="/src/static/css/main.css" rel="stylesheet"/>
+<link href="/src/static/css/header.css" rel="stylesheet"/>
+<link href="/src/static/css/footer.css" rel="stylesheet"/>
+<link href="/src/static/css/validation.css" rel="stylesheet"/>
+<link href="/src/static/css/spinner.css" rel="stylesheet"/>
 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
