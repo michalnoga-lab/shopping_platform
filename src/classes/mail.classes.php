b2d0b64a768e1dc86c3948cf913d9151e0fccf82
Michal <michalnoga.poczta@gmail.com>
2022-04-11 11:17:17 +0200

    Email send configuration

--- src/classes/mail.classes.php
+++ src/classes/mail.classes.php
@@ -1,12 +1,29 @@
 <?php
 // error_reporting(0); // TODO
 
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+
+require_once '../../vendor/autoload.php';
+include_once '../../.data.php';
 
 class MailBot
 {
 
     public function sendEmail($to)
     {
+        $email = (new Email())
+            ->from('pocztazamowienauto@gmail.com')
+            ->to('biuro@primakrakow.pl')
+            ->subject('Rejestracja')
+            ->text('Dziala');
 
+        $credentials = new Credentials();
+        $transport = Transport::fromDsn($credentials->getMailTransportGoogleDsn());
+
+        $mailer = new Mailer($transport);
+
+        $mailer->send($email);
     }
 }
